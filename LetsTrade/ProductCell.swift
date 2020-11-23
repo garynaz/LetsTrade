db27f42fea3c9efbeadf79de41ed25d87bb64613
garynaz <garynaz@icloud.com>
2020-11-22 20:59:38 -0500

    Ready to prepare cell for datasource integration.

--- LetsTrade/ProductCell.swift
+++ LetsTrade/ProductCell.swift
@@ -12,6 +12,14 @@ class ProductCell: UICollectionViewCell {
     
    static let cellId = "ProductCellId"
     
+   private let deleteButton :UIButton = {
+        let deleteButton = UIButton()
+        deleteButton.setImage(UIImage(systemName: "multiply.square.fill"), for: .normal)
+        deleteButton.addTarget(ViewController.self, action: #selector(deleteItem), for: .touchUpInside)
+        return deleteButton
+    }()
+    
+    
     private let myImageView : UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "photo")
@@ -44,8 +52,8 @@ class ProductCell: UICollectionViewCell {
         contentView.layer.borderWidth = 1
         contentView.layer.borderColor = UIColor.systemGray.cgColor
         contentView.layer.cornerRadius = 10
-        contentView.backgroundColor = .systemGray3
-        [myImageView, titleLabel, priceLabel].forEach{addSubview($0)}
+        contentView.backgroundColor = .white
+        [myImageView, titleLabel, priceLabel, deleteButton].forEach{addSubview($0)}
     }
     
     required init?(coder: NSCoder) {
@@ -55,8 +63,9 @@ class ProductCell: UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        deleteButton.anchor(top: contentView.topAnchor, leading: myImageView.trailingAnchor, bottom: titleLabel.topAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 0, left: 80, bottom: 20, right: 0))
         myImageView.anchor(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 120))
-        titleLabel.anchor(top: contentView.topAnchor, leading: myImageView.trailingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 40, left: 0, bottom: 120, right: 0))
+        titleLabel.anchor(top: contentView.topAnchor, leading: myImageView.trailingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 60, left: 0, bottom: 90, right: 0))
         priceLabel.anchor(top: contentView.topAnchor, leading: myImageView.trailingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 100, left: 0, bottom: 20, right: 0))
     }
     
@@ -68,6 +77,10 @@ class ProductCell: UICollectionViewCell {
         priceLabel.font = priceLabel.font.withSize(20)
         
         myImageView.image = UIImage(named: image)
+    }
+    
+   @objc func deleteItem(){
+        //Code for deleting cell/item.
     }
     
     override func prepareForReuse() {
